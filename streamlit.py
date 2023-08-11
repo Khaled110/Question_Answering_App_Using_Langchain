c0032b8c074bdfc20c07c81b968657382dd4f686
Khaled Nabil <49573699+Khaled110@users.noreply.github.com>
2023-08-11 23:39:25 +0300

    Add files via upload

--- streamlit.py
+++ streamlit.py
@@ -1,9 +1,37 @@
 import streamlit as st
-from langchain.llms import OpenAI
+#from langchain.llms import OpenAI
+from langchain import PromptTemplate, LLMChain, HuggingFaceHub
+import os
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
 
 st.title('🦜🔗 Quickstart App')
 
+def generate_response(question):
+  repo_id = "bigscience/bloom"  
+  llm=HuggingFaceHub(repo_id=repo_id, model_kwargs={"temperature":1e-10})
+  llm_chain = LLMChain(llm=llm, prompt=prompt)
+  answer = llm_chain.run(question).split('\n')[0]
+  st.info(answer)
+
+template = """Question: {question}
+
+Answer:"""
+prompt = PromptTemplate(
+        template=template,
+    input_variables=['question']
+)
+
+with st.form('my_form'):
+  text = st.text_area('Enter your question:', 'who was founded Facebook?')
+  submitted = st.form_submit_button('Submit')
+  if submitted:
+    generate_response(text)
+
+
+"""
 openai_api_key = st.sidebar.text_input('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')
+
+
 
 def generate_response(input_text):
   llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
@@ -16,4 +44,6 @@ with st.form('my_form'):
     st.warning('Please enter your OpenAI API key!', icon='⚠')
   if submitted and openai_api_key.startswith('sk-'):
     generate_response(text)
+
+"""
 
